e85fe5c2664297d81a0a38985a52af85ca2cf5d5
Srilekha <srilekhams817@gmail.com>
2023-02-25 14:49:08 +0530

    Landing-page final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,20 @@
-const header = document.getElementById("#header");
+const header = document.getElementById("header");
 const nav = document.getElementById("nav");
+const toggle_menu = document.querySelector(".show-menu");
+const hiddenMenu = document.querySelector(".hidden-menu");
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) nav.classList.add("sticky");
+      else nav.classList.remove("sticky");
+    });
+  },
+  {
+    threshold: 0,
+  }
+);
+console.log(header);
+observer.observe(header);
+toggle_menu.addEventListener("click", () =>
+  hiddenMenu.classList.toggle("show")
+);
