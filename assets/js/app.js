b198168715122d8f10e8b609617a826c178a19de
Srilekha <srilekhams817@gmail.com>
2023-02-28 13:36:59 +0530

    Changes in responsive layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,39 @@
 const toggle_menu = document.querySelector(".show-menu");
 const hiddenMenu = document.querySelector(".hidden-menu");
-const root = document.getElementById('root');
+const footer = document.getElementById("front-footer");
+const mobile_footer = document.getElementById("mobile-footer");
+const mobile_header = document.getElementById("mobile-header");
+const card = document.getElementById("first-card");
+const main = document.getElementById("mobile-main");
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) footer.classList.add("hide");
+      else footer.classList.remove("hide");
+    });
+  },
+  {
+    rootMargin: "130px",
+    threshold: 0.2,
+  }
+);
+observer.observe(main);
+
+const cardObserver = new IntersectionObserver(
+  (entries, cardObserver) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (!entry.isIntersecting) mobile_footer.classList.add("hide");
+      else mobile_footer.classList.remove("hide");
+    });
+  },
+  {
+    rootMargin: "-20px",
+    threshold: 1,
+  }
+);
+cardObserver.observe(card);
 
 //Close menu when clicked outside
 document.addEventListener("click", (e) => {
@@ -12,4 +45,3 @@ document.addEventListener("click", (e) => {
 toggle_menu.addEventListener("click", () =>
   hiddenMenu.classList.toggle("show")
 );
-
