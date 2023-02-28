9a9a077ab557e14567af7afa35cd6de9b930671e
Srilekha <srilekhams817@gmail.com>
2023-02-28 10:04:12 +0530

    mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,15 @@
-const header = document.getElementById("header");
-const nav = document.getElementById("nav");
 const toggle_menu = document.querySelector(".show-menu");
 const hiddenMenu = document.querySelector(".hidden-menu");
-const observer = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) nav.classList.add("sticky");
-      else nav.classList.remove("sticky");
-    });
-  },
-  {
-    threshold: 0,
+const root = document.getElementById('root');
+
+//Close menu when clicked outside
+document.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("menu-toggler")) {
+    hiddenMenu.classList.remove("show");
   }
-);
-console.log(header);
-observer.observe(header);
+});
+
 toggle_menu.addEventListener("click", () =>
   hiddenMenu.classList.toggle("show")
 );
+
