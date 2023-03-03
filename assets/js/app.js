4e3333e1b76c61c63283db6dd88751dc2d552676
Srilekha <srilekhams817@gmail.com>
2023-03-03 16:13:00 +0530

    Changes in widgets

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,46 @@ const mobile_header = document.getElementById("mobile-header");
 const card = document.getElementById("first-card");
 const main = document.getElementById("mobile-main");
 const logo = document.getElementById("logo");
-const rightBtn = document.getElementById("right-chevron");
-const leftChevron = document.getElementById("left-chevron");
+const rightBtn = document.getElementById("rightChevron");
+const leftChevron = document.getElementById("leftChevron");
+const widgetShow = document.getElementById("widgets-show");
+const anyweekbtn = document.getElementById("anyweek");
+const menu_hidden = document.getElementById("hidden-nav");
+const checkbtn = document.getElementById("checkIn-btn");
+const dateShow = document.getElementById("dates-show");
+const countryShow = document.getElementById("countries-show");
+const wherebtn = document.getElementById("wherebtn");
+const body = document.getElementById("content-body");
+
+wherebtn.onclick = () => {
+  countryShow.style.display = "block";
+  dateShow.style.display = "none";
+};
+anyweekbtn.onclick = () => {
+  menu_hidden.style.display = "block";
+};
+checkbtn.onclick = () => {
+  countryShow.style.display = "none";
+  dateShow.style.display = "block";
+};
+
+widgetShow.onclick = () => {
+  menu_hidden.style.display = "block";
+  document.getElementById("content-body").style.opacity = 0.95;
+  document.getElementById("content-body").style.background = "rgba(0,0,0,0.3)";
+};
+
+rightBtn.onclick = () => {
+  console.log("chevron clicked");
+  leftChevron.style.display = "block";
+  document.getElementById("navlist-items").scrollLeft += 500;
+};
+
+leftChevron.addEventListener("click", function (event) {
+  document.getElementById("navlist-items").scrollLeft -= 1000;
+  event.preventDefault();
+  leftChevron.style.display = "none";
+});
 
 window.addEventListener("resize", () => {
   if (window.innerWidth <= 950) logo.src = "assets/images/logo_small.png";
